d038e665d8ac5cfc2d7fbf829103e3514b85d641
missi0415 <missi0415@gmail.com>
2021-08-13 20:28:42 +0900

    test追加

--- api/test/test_helper.rb
+++ api/test/test_helper.rb
@@ -2,12 +2,22 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+
+# gem 'minitest-reporters' setup
+require "minitest/reporters"
+Minitest::Reporters.use!
+# ここまで
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+# プロセスが分岐した直後に呼び出し
+parallelize_setup do |worker|
+  load "#{Rails.root}/db/seeds.rb"
+end
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+parallelize(workers: :number_of_processors)
 
-  # Add more helper methods to be used by all tests here...
+  # 追加
+  def active_user
+    User.find_by(activated: true)
+  end
+
 end
