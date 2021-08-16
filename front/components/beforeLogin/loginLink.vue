c614726b54cbc68936aa449d1cb4724e28421c0b
missi0415 <missi0415@gmail.com>
2021-08-16 22:11:13 +0900

    randingpage finsh

--- front/components/beforeLogin/loginLink.vue
+++ front/components/beforeLogin/loginLink.vue
@@ -1,9 +1,15 @@
 <template>
-  <v-btn text class="ml-2 font-weight-bold" color="black" to="/login">
-    {{ $t("pages.login") }}
+  <v-btn
+    text
+    class="ml-2 font-weight-bold"
+    color="black"
+    to="/login"
+  >
+    {{ $t('pages.login') }}
   </v-btn>
 </template>
 
 <script>
-export default {};
+export default {
+}
 </script>
